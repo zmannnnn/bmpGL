8e72685de2608418547a318800d61b42e53ec8e4
zengcheng <www.zc125678@163.com>
2020-12-08 16:33:54 +0800

    添加子组件

---
+++ src/components/bmpGL.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="bmpGL">
+    <div id="container" class="allmap"></div>
+  </div>
+</template>
+
+<script>
+import { BMPGL } from "@/bmpgl.js"
+export default {
+  name: "bmpGL",
+  data() {
+    return {
+      ak: "LnP4INwY2AY2VSZFwcRMbNhPxUeBB7sA", // 这里就是百度地图密钥
+      myMap: null
+    };
+  },
+  mounted() {
+    this.initMap()
+  },
+  methods: {
+    initMap() {
+      BMPGL(this.ak).then((BMapGL) => {
+        // 创建地图实例
+        let map = new BMapGL.Map("container");
+        // 创建点坐标 axios => res 获取的初始化定位坐标
+        let point = new BMapGL.Point(114.031761, 22.542826)
+        // 初始化地图，设置中心点坐标和地图级别
+        map.centerAndZoom(point, 19)
+        //开启鼠标滚轮缩放
+        map.enableScrollWheelZoom(true)
+        map.setHeading(64.5)
+        map.setTilt(73)
+        // 保存数据
+        // this.myMap = map
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    },
+  }
+};
+</script>
+<style lang="scss" scoped>
+.allmap {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  z-index: 1;
+}
+</style>
