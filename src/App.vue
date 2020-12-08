826c7e6a2af94927bb92b4d4972273a9ed7dab38
zengcheng <www.zc125678@163.com>
2020-12-08 16:31:51 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <bmpGL />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import bmpGL from './components/bmpGL.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    bmpGL
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  border: 0;
+  outline: none;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
